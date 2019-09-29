dc7a45ddedaa874cd0f6e72de9fff6a37a86bab2
Peter Gyory <ptgyory@gmail.com>
2019-09-28 18:13:50 -0600

    Update QTMConnector.cs

--- QTMConnector.cs
+++ QTMConnector.cs
@@ -5,20 +5,22 @@ using QTMRealTimeSDK;
 using QTMRealTimeSDK.Data;
 using QualisysRealTime.Unity;
 
+// This class automatically connects to the Qualisys server on startup
 public class QTMConnector : MonoBehaviour
 {
-    public string host;
+    public string host; // The ipaddress of the host server, make sure you set this in the editor
     private DiscoveryResponse server;
     private bool foundServer = false;
     private RTClient rtClient;
 
-    // Start is called before the first frame update
     void Start()
     {
+        // Get the global RTClient object from the Qualisys system
         rtClient = RTClient.GetInstance();
     
         // Loop through discovered servers to find one that matches our needs
         foreach (var discoveryResponse in rtClient.GetServers()) {
+            // Check if the response has the right IP address
             if (discoveryResponse.IpAddress == host) {
                 Debug.Log("Desired Host Found");
                 server = discoveryResponse;
@@ -26,7 +28,7 @@ public class QTMConnector : MonoBehaviour
             }
         }
 
-        // Maybe do something on update to attempt again?
+        // If a server is found at the correct host, connect to the server
         if (foundServer) {
             rtClient.Connect(server, 4545, true, true, false, false, false, false);
         }
